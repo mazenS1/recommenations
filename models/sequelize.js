d5a62ae7a49c82b55ef0b4650d495ead8d225801
mazen <mazen.saleh.36@gmail.com>
2024-12-14 21:00:09 +0300

    try

--- models/sequelize.js
+++ models/sequelize.js
@@ -1,21 +1,32 @@
 // models/sequelize.js
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    },
-    logging: process.env.NODE_ENV === 'development',
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
+  dialect: 'postgres',
+  logging: false, // Fixes the deprecation warning
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
+
+// Test the connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
