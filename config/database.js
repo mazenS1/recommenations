f267bd828fb6fbf2b61002e2d651e57f84c59ab3
mazen <mazen.saleh.36@gmail.com>
2024-12-15 20:17:20 +0300

    for

--- config/database.js
+++ config/database.js
@@ -1,16 +1,7 @@
 require('dotenv').config();
 module.exports = {
   development: {
-    username: 'neondb_owner',
-    password: 'BHVpN9aX7RIZ',
-    database: 'neondb',
-    host: 'ep-cool-heart-a2quf4r7-pooler.eu-central-1.aws.neon.tech',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true
-      }
-    }
-  },
-  // Add other environments if needed (production, test, etc.)
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres'
+  }
 };
